e2dd4a3d41ceb5c87a2417a22f3304b6609a5901
MajedMarkSalem <90328142+MajedMarkSalem@users.noreply.github.com>
2021-10-21 18:00:50 +0200

    Create majed.sh

---
+++ majed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+  
+# Read the user input   
+  
+echo "Enter the first name: "  
+read first_name  
+echo "Enter the last name: "  
+read last_name  
+
+echo "The Current User is $first_name $last_name"  
+echo  
