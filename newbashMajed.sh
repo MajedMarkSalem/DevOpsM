7deb81bcd104d64fdad96f091d0be461b2fc80de
MajedMarkSalem <90328142+MajedMarkSalem@users.noreply.github.com>
2021-12-19 19:54:10 +0100

    added new bash file

---
+++ newbashMajed.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+  
+# Read the user input   
+  
+echo "Enter the first name: "  
+read first_name  
+echo "Enter the last name: "  
+read last_name  
+echo "Enter your age:"
+read age
+
+echo "The Current User is $first_name $last_name and your age is $age"  
+echo  
